96af337e6f68e2575b75891276619622f08e932f
Carlos Garita <garitadaniel@hotmail.com>
2026-02-11 03:39:17 +0100

    Botón de logout agregado a portal de super-admin

--- app/(dashboard)/super-admin/layout.tsx
+++ app/(dashboard)/super-admin/layout.tsx
@@ -70,6 +70,16 @@ async function SuperAdminLayout({ children }: PropsWithChildren) {
                 </Link>
               </div>
             </div>
+            <div className="hidden sm:ml-6 sm:flex sm:items-center">
+              <form action="/auth/signout" method="post">
+                <button
+                  type="submit"
+                  className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                >
+                  Cerrar Sesión
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </nav>
